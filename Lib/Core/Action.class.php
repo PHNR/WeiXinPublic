abeecdf339bb1afcb84b0ab4459e423d7f76c11e
SprayHank <fantasyoui@gmail.com>
2014-08-21 16:02:35 +0800

    整理代码

--- Lib/Core/Action.class.php
+++ Lib/Core/Action.class.php
@@ -11,10 +11,10 @@
 
 /**
  * ThinkPHP Action控制器基类 抽象类
- * @category   Think
- * @package  Think
+ * @category    Think
+ * @package     Think
  * @subpackage  Core
- * @author   liu21st <liu21st@gmail.com>
+ * @author      liu21st <liu21st@gmail.com>
  */
 abstract class Action {
 
@@ -89,12 +89,14 @@ abstract class Action {
     /**
      * 模板显示 调用内置的模板引擎显示方法，
      * @access protected
+     *
      * @param string $templateFile 指定要调用的模板文件
-     * 默认为空 由系统自动定位模板文件
-     * @param string $charset 输出编码
-     * @param string $contentType 输出类型
-     * @param string $content 输出内容
-     * @param string $prefix 模板缓存前缀
+     *                             默认为空 由系统自动定位模板文件
+     * @param string $charset      输出编码
+     * @param string $contentType  输出类型
+     * @param string $content      输出内容
+     * @param string $prefix       模板缓存前缀
+     *
      * @return void
      */
     protected function display($templateFile = '', $charset = '', $contentType = '', $content = '', $prefix = '') {
@@ -105,9 +107,11 @@ abstract class Action {
     /**
      * 输出内容文本可以包括Html 并支持内容解析
      * @access protected
-     * @param string $content 输出内容
-     * @param string $charset 模板输出字符集
+     *
+     * @param string $content     输出内容
+     * @param string $charset     模板输出字符集
      * @param string $contentType 输出类型
+     *
      * @return mixed
      */
     protected function show($content, $charset = '', $contentType = '') {
@@ -119,8 +123,10 @@ abstract class Action {
      *  获取输出页面内容
      * 调用内置的模板引擎fetch方法，
      * @access protected
+     *
      * @param string $templateFile 指定要调用的模板文件
-     * 默认为空 由系统自动定位模板文件
+     *                             默认为空 由系统自动定位模板文件
+     *
      * @return string
      */
     protected function fetch($templateFile = '') {
@@ -142,11 +148,13 @@ abstract class Action {
 
     /**
      *  创建静态页面
-     * @access protected
+     * @access   protected
      * @htmlfile 生成的静态文件名称
      * @htmlpath 生成的静态文件路径
+     *
      * @param string $templateFile 指定要调用的模板文件
-     * 默认为空 由系统自动定位模板文件
+     *                             默认为空 由系统自动定位模板文件
+     *
      * @return string
      */
     protected function buildHtml($htmlfile = '', $htmlpath = '', $templateFile = '') {
@@ -164,8 +172,10 @@ abstract class Action {
     /**
      * 模板变量赋值
      * @access protected
-     * @param mixed $name 要显示的模板变量
+     *
+     * @param mixed $name  要显示的模板变量
      * @param mixed $value 变量的值
+     *
      * @return void
      */
     protected function assign($name, $value = '') {
@@ -183,7 +193,9 @@ abstract class Action {
     /**
      * 取得模板显示变量的值
      * @access protected
+     *
      * @param string $name 模板显示变量
+     *
      * @return mixed
      */
     public function get($name = '') {
@@ -209,8 +221,10 @@ abstract class Action {
     /**
      * 魔术方法 有不存在的操作的时候执行
      * @access public
+     *
      * @param string $method 方法名
-     * @param array $args 参数
+     * @param array  $args   参数
+     *
      * @return mixed
      */
     public function __call($method, $args) {
@@ -303,9 +317,11 @@ abstract class Action {
     /**
      * 操作错误跳转的快捷方法
      * @access protected
-     * @param string $message 错误信息
-     * @param string $jumpUrl 页面跳转地址
-     * @param Boolean|array $ajax 是否为Ajax方式
+     *
+     * @param string        $message 错误信息
+     * @param string        $jumpUrl 页面跳转地址
+     * @param Boolean|array $ajax    是否为Ajax方式
+     *
      * @return void
      */
     protected function error($message, $jumpUrl = '', $ajax = false) {
@@ -315,9 +331,11 @@ abstract class Action {
     /**
      * 操作成功跳转的快捷方法
      * @access protected
-     * @param string $message 提示信息
-     * @param string $jumpUrl 页面跳转地址
-     * @param Boolean|array $ajax 是否为Ajax方式
+     *
+     * @param string        $message 提示信息
+     * @param string        $jumpUrl 页面跳转地址
+     * @param Boolean|array $ajax    是否为Ajax方式
+     *
      * @return void
      */
     protected function success($message, $jumpUrl = '', $ajax = false) {
@@ -326,8 +344,10 @@ abstract class Action {
     /**
      * Ajax方式返回数据到客户端
      * @access protected
-     * @param mixed $data 要返回的数据
+     *
+     * @param mixed  $data 要返回的数据
      * @param String $type AJAX返回数据格式
+     *
      * @return void
      */
     protected function ajaxReturn($data, $type = '') {
@@ -390,10 +410,12 @@ abstract class Action {
     /**
      * Action跳转(URL重定向） 支持指定模块和延时跳转
      * @access protected
-     * @param string $url 跳转的URL表达式
-     * @param array $params 其它URL参数
-     * @param integer $delay 延时跳转的时间 单位为秒
-     * @param string $msg 跳转提示信息
+     *
+     * @param string  $url    跳转的URL表达式
+     * @param array   $params 其它URL参数
+     * @param integer $delay  延时跳转的时间 单位为秒
+     * @param string  $msg    跳转提示信息
+     *
      * @return void
      */
     protected function redirect($url, $params = array(), $delay = 0, $msg = '') {
@@ -405,10 +427,12 @@ abstract class Action {
      * 默认跳转操作 支持错误导向和正确跳转
      * 调用模板显示 默认为public目录下面的success页面
      * 提示页面为可配置 支持模板标签
-     * @param string $message 提示信息
-     * @param Boolean $status 状态
-     * @param string $jumpUrl 页面跳转地址
-     * @param Boolean|array $ajax 是否为Ajax方式
+     *
+     * @param string        $message 提示信息
+     * @param Boolean       $status  状态
+     * @param string        $jumpUrl 页面跳转地址
+     * @param Boolean|array $ajax    是否为Ajax方式
+     *
      * @access private
      * @return void
      */
